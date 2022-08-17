7a277429962e9c188b5c3415d38cd367c2445b78
주금석 <kalkin@mz.co.kr>
2022-08-17 09:32:19 +0900

    주석제거

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router';
-// import LoginPage from '@/views/LoginPage';
-// import SignupPage from '../views/SignupPage.vue';
 
 const routes = [
 	{
-		path: '/',
-		//코드 스플리팅 적용.
-		component: () => import('@/views/LoginPage.vue'),
-	},
-	{
 		path: '/login',
-		// component: LoginPage,
 		component: () => import('@/views/LoginPage.vue'),
 	},
 	{
 		path: '/signup',
-		// component: SignupPage,
 		component: () => import('@/views/SignupPage.vue'),
 	},
 ];
