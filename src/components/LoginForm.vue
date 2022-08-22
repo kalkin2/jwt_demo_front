6601df0c89650ca2be6bf90f075ae0f756db8e0c
주금석 <kalkin@mz.co.kr>
2022-08-22 15:42:15 +0900

    로그인폼추가

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,29 @@
+<template>
+	<form @submit.prevent="submitForm">
+		<div>
+			<label for="username">ID: </label>
+			<input id="username" type="text" v-model="username" />
+		</div>
+		<div>
+			<label for="password">PASSWORD: </label>
+			<input id="password" type="text" v-model="password" />
+		</div>
+		<button type="submit">로그인</button>
+	</form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			username: '',
+			password: '',
+		};
+	},
+	methods: {
+		submitForm() {},
+	},
+};
+</script>
+
+<style></style>
