8c6f87298258e339a4509a8e459bfa207e16877b
주금석 <kalkin@mz.co.kr>
2022-08-22 15:17:33 +0900

    axios config

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
+const instance = axios.create({
+	baseURL: 'http://localhost:3000/',
+});
 function registerUser(userData) {
-	const url = 'http://localhost:3000/signup';
-	return axios.post(url, userData);
+	// const url = 'http://localhost:3000/signup';
+	// return axios.post(url, userData);
+	return instance.post('signUp', userData);
 }
+
+// function loginUser(userData) {
+// 	const url = 'http://localhost:3000/login';
+// 	return axios.post(url, userData);
+// }
 
 export { registerUser };
