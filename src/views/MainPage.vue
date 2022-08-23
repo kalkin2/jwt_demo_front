00147bbf4a61b8a5705b1d9688121ac8c9e46334
주금석 <kalkin@mz.co.kr>
2022-08-23 16:04:13 +0900

    글조회 api추가.

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,11 +1,33 @@
 <template>
 	<div class="main list-container contents">
 		<h1 class="page-header">Today I Learn. Main Page</h1>
+		<ul>
+			<li v-for="postItem in postItems" :key="postItem._id">
+				{{ postItem.contents }}
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index';
+export default {
+	data() {
+		return {
+			postItems: [],
+		};
+	},
+	methods: {
+		async fetchNotes() {
+			const { data } = await fetchPosts();
+			this.postItems = data.posts;
+			console.log('posts:::', this.postItems);
+		},
+	},
+	created() {
+		this.fetchNotes();
+	},
+};
 </script>
 
 <style></style>
